bc556fd090d7b48ffcb05e3448ed90cfb57321ea
rasmus234 <rasmussonntag@gmail.com>
2022-06-07 12:50:26 +0200

    add test class

--- PlantControl.Models/LoggerConfig.cs
+++ PlantControl.Models/LoggerConfig.cs
@@ -7,11 +7,24 @@ public class LoggerConfig
     public Soil Soil { get; set; }
 }
 
+public record class Logging(string Id, bool Active, string HubUrl, string RestUrl)
+{
+    public string Id { get; } = Id;
+    public bool Active { get; } = Active;
+    public string HubUrl { get; } = HubUrl;
+    public string RestUrl { get; } = RestUrl;
+}
 
+public record class Air(float MinHumid, float MaxHumid, float MinTemp, float MaxTemp)
+{
+    public float MinHumid { get; } = MinHumid;
+    public float MaxHumid { get; } = MaxHumid;
+    public float MinTemp { get; } = MinTemp;
+    public float MaxTemp { get; } = MaxTemp;
+}
 
-
-public record class Logging(string Id, bool Active, string HubUrl, string RestUrl);
-
-public record class Air(float MinHumid, float MaxHumid, float MinTemp, float MaxTemp);
-
-public record class Soil(float Moist, float Dry);
+public record class Soil(float Moist, float Dry)
+{
+    public float Moist { get; } = Moist;
+    public float Dry { get; } = Dry;
+}
